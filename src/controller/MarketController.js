73bd2b6447225797426f7e5d0e1f99d58c5972e7
Italo <italocmelo_@hotmail.com>
2019-10-07 10:00:14 -0300

    Market Register method

--- src/controller/MarketController.js
+++ src/controller/MarketController.js
@@ -1,14 +1,33 @@
 //index "lista de objetos" , show "mostra um objeto", store "Cria um objeto", update "Alterar objeto", destroy "Deletar objeto"
 const Market = require('../model/Market');
+const User = require('../model/User');
 
 module.exports = {
     async store(req, res) {
 
         const { filename } = req.file;
         const { name , adress , active } = req.body;
+        const { user_id } = req.headers;
 
-        //const market = await Market.create(req.body)
+        const user = await User.findById(user_id);
 
-        return res.json({result : true});
+        console.log(adress);
+
+        if(!user){
+            return res.status(400).json({
+                error : 'User not found.'
+            });
+        }
+
+        const market = await Market.create({
+            user : user_id,
+            name : name,
+            image : filename,
+            adress : JSON.parse(adress),
+            active : active
+
+        })
+
+        return res.json({ market });
     }
 };
